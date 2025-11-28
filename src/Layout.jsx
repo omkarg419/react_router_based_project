bd4b8a2d725dc67b928fe66d39e6cb8d906d3906
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-11-28 12:02:34 +0530

    feat: implement Layout component with Header and Footer imports

---
+++ src/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return <div></div>;
+}
+
+export default Layout;
